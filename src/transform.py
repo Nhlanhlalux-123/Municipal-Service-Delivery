23f07715a15224e8a20ca5728fa4eea3101f251a
Nhlanhla Mabanga <nhmabjhb025@student.wethinkcode.co.za>
2026-09-14 12:01:13 +0200

    updated the clean_data function to remove duplicate records

--- src/transform.py
+++ src/transform.py
@@ -1,7 +1,13 @@
 def clean_data(rows):
     cleaned_rows = []
+    seen_ids = set()
 
     for row in rows:
+        if row["id"] in seen_ids:
+            continue
+
+        seen_ids.add(row["id"])
+
         municipality = row["municipality"].strip().title()
         service = row["service"].strip().title()
         area = row["area"].strip.title()
