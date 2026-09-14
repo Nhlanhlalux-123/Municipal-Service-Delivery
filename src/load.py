1b0b7a67af25686a162ee029ecc66d9e7afad4b4
Nhlanhla Mabanga <nhmabjhb025@student.wethinkcode.co.za>
2026-09-14 13:58:23 +0200

    created a file that will connect and add data to the database

---
+++ src/load.py
@@ -0,0 +1,22 @@
+import sqlite3
+
+DATABASE = "database/service_data.db"
+
+def create_database():
+    connection = sqlite3.connect(DATABASE)
+
+    cursor = connection.cursor()
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS service_requests (
+        id INTEGER PRIMARY KEY,
+        date TEXT NOT NULL,
+        municipality TEXT NOT NULL,
+        service TEXT NOT NULL,
+        area TEXT NOT NULL,
+        status TEXT NOT NULL 
+        )
+    """)
+
+    connection.commit
+    connection.close()
