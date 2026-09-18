e89f17ac63e69a90fda4dd5ea4c7066ccec21df2
Nhlanhla Mabanga <nhmabjhb025@student.wethinkcode.co.za>
2026-09-18 11:34:08 +0200

    containerized the Pipeline

--- src/database.py
+++ src/database.py
@@ -8,7 +8,7 @@ load_dotenv()
 
 
 DATABASE_CONFIG = {
-    "host": os.getenv("POSTGRES_HOST"),
+    "host": os.getenv("POSTGRES_HOST", "localhost"),
     "port": os.getenv("POSTGRES_PORT"),
     "dbname": os.getenv("POSTGRES_DB"),
     "user": os.getenv("POSTGRES_USER"),
